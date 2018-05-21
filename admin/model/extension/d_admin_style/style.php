de83333d9a9c1692462549caebbf61ea5d51edf9
pavel <php5.drvn@gmail.com>
2018-05-21 12:43:22 +0300

    add support old version admin style

--- admin/model/extension/d_admin_style/style.php
+++ admin/model/extension/d_admin_style/style.php
@@ -1,20 +1,27 @@
 <?php
 
-class ModelExtensionDAdminStyleStyle extends Model {
+class ModelExtensionDAdminStyleStyle extends Model
+{
 
-    public function getStyles ($theme_name){
-$this->getAdminStyle($theme_name);
+    public function getStyles($theme_name)
+    {
+        $this->getAdminStyle($theme_name);
     }
-    public function getAdminStyle($theme_name){
+
+    public function getAdminStyle($theme_name)
+    {
         $this->document->addStyle('view/stylesheet/d_admin_style/core/normalize/normalize.css');
-        $this->document->addStyle('view/stylesheet/d_admin_style/themes/'.$theme_name.'/styles.css');
+        $this->document->addStyle('view/stylesheet/d_admin_style/themes/' . $theme_name . '/styles.css');
 
     }
-    public function getAvailableThemes(){
+
+    public function getAvailableThemes()
+    {
         $dir = DIR_APPLICATION . 'view/stylesheet/d_admin_style/themes';
         $name_dirs = scandir($dir);
-        return  array_diff($name_dirs, array('.', '..'));
+        return array_diff($name_dirs, array('.', '..'));
     }
+
     public function getLanguageText($data)
     {
         $this->language->load('extension/d_admin_style/style');

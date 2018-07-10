399e1544b3d38eec7f787d8aab6809e0b68ee534
pavel <php5.drvn@gmail.com>
2018-07-10 14:55:32 +0300

    fixing many issues with modules (blog, social, checkout7,visual designer, ajax_filter)

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,13 @@ var gulp           = require('gulp'),
 // Обновление страниц сайта на локальном сервере
 gulp.task('browser-sync', function() {
     browserSync({
-        proxy: "localhost",
+        proxy: "http://localhost/302/d_toolkit",
         notify: true
     });
 });
 // will compille styles in dark and light folders
 gulp.task('sass', function () {
-    return gulp.src('admin/view/stylesheet/d_admin_style/**/styles.scss')
+    return gulp.src(['admin/view/stylesheet/d_admin_style/**/styles.s*ss','admin/view/stylesheet/d_admin_style/**/modification/*.s*ss',])
         .pipe(autoprefixer(['last 15 versions']))
         .pipe(sass().on('error', sass.logError))
         .pipe(cleanCSS())
@@ -22,6 +22,7 @@ gulp.task('sass', function () {
 });
 // Наблюдение за файлами
 gulp.task('watch', ['sass', 'browser-sync'], function() {
+    gulp.watch('admin/view/stylesheet/d_admin_style/themes/**/modification/**.scss', ['sass']);
     gulp.watch('admin/view/stylesheet/d_admin_style/themes/**/styles.scss', ['sass']);
     gulp.watch('admin/view/stylesheet/d_admin_style/core/**/*.scss', ['sass']);
     gulp.watch('admin/view/template/extension/module/**/*.twig', browserSync.reload);
